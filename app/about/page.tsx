27504df66379a7fc342b472790e3c4d567fccd60
Rifat-noor <rifatnoor9092@gmail.com>
2025-11-11 13:27:01 +0600

    about page change

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AboutPage() {
     return (
         <div>
-            <h1>this is my about page</h1>
+            <h1>this is my about page again updated</h1>
         </div>
     )
 }
